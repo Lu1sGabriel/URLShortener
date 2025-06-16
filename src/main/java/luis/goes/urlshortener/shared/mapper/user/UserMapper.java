cf0d9dff4e4b6c6f7d175aab4f866ecc411ed39e
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-06-15 23:31:33 -0300

    Feat: Criação da classe que faz o Mapper entre `UserEntity` e seus DTOs

---
+++ src/main/java/luis/goes/urlshortener/shared/mapper/user/UserMapper.java
@@ -0,0 +1,46 @@
+package luis.goes.urlshortener.shared.mapper.user;
+
+import luis.goes.urlshortener.domain.entity.user.UserEntity;
+import luis.goes.urlshortener.presentation.dtos.user.UserRequestDTO;
+import luis.goes.urlshortener.presentation.dtos.user.UserResponseDto;
+import luis.goes.urlshortener.shared.mapper.IMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public final class UserMapper implements IMapper<UserRequestDTO, UserResponseDto, UserEntity> {
+
+    @Override
+    public UserEntity toEntity(UserRequestDTO dto) {
+        UserEntity user = new UserEntity();
+        user.setName(dto.name());
+        return user;
+    }
+
+    @Override
+    public UserResponseDto toDto(UserEntity userEntity) {
+        return new UserResponseDto(
+                userEntity.getId(),
+                userEntity.getName(),
+                userEntity.getDateInfo().getCreatedAt(),
+                userEntity.getDateInfo().getUpdatedAt()
+        );
+    }
+
+    @Override
+    public List<UserEntity> toEntityList(List<UserRequestDTO> userRequestDTOS) {
+        return userRequestDTOS.stream()
+                .map(this::toEntity)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<UserResponseDto> toDtoList(List<UserEntity> userEntities) {
+        return userEntities.stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
+
+}
