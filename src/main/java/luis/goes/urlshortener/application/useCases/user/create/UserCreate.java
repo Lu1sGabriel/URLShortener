a4ef476cd61ff8cf9e072c6059b26ef1c4c1d4d1
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-06-15 23:34:06 -0300

    Feat: Criação do `UserCreate`

---
+++ src/main/java/luis/goes/urlshortener/application/useCases/user/create/UserCreate.java
@@ -0,0 +1,33 @@
+package luis.goes.urlshortener.application.useCases.user.create;
+
+import luis.goes.urlshortener.domain.entity.user.UserEntity;
+import luis.goes.urlshortener.infrastructure.repository.user.UserRepository;
+import luis.goes.urlshortener.presentation.dtos.user.UserResponseDto;
+import luis.goes.urlshortener.shared.mapper.user.UserMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserCreate implements IUserCreateUseCase {
+
+    private final UserRepository repository;
+    private final UserMapper mapper;
+
+    public UserCreate(UserRepository repository, UserMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public final UserResponseDto create(String name) {
+        if (name == null) {
+            return null;
+        }
+
+        UserEntity user = new UserEntity(name);
+
+        repository.save(user);
+
+        return mapper.toDto(repository.save(user));
+    }
+
+}
