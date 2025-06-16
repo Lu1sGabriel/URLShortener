d771955452a8a14c0cf90870fd52bf65225f2161
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-06-15 22:49:42 -0300

    Refactor: Refatoração de `record` para `class`

--- src/main/java/luis/goes/urlshortener/domain/entity/url/URLEntity.java
+++ src/main/java/luis/goes/urlshortener/domain/entity/url/URLEntity.java
@@ -1,24 +1,41 @@
 package luis.goes.urlshortener.domain.entity.url;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import luis.goes.urlshortener.domain.entity.user.UserEntity;
 
 import java.util.UUID;
 
 @Table
 @Entity(name = "url_db")
-public record URLEntity(
-        @Id UUID id,
-        @Column(name = "original_url", nullable = false) String original,
-        @Column(name = "shortened_url", nullable = false) String shortened,
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "user_id", nullable = false)
-        UserEntity user
-) {
-    public URLEntity {
+@Getter
+@Setter
+public class URLEntity {
+
+    @Id
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+
+    @Column(name = "original_url", nullable = false)
+    private String original;
+
+    @Column(name = "shortened_url", nullable = false)
+    private String shortened;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    public URLEntity() {
     }
 
     public URLEntity(String original, String shortened, UserEntity user) {
-        this(UUID.randomUUID(), original, shortened, user);
+        this.id = UUID.randomUUID();
+        this.original = original;
+        this.shortened = shortened;
+        this.user = user;
     }
+
 }
