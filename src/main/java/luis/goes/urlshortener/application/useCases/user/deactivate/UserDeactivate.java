09402bc2f8a8b93e096be1c69d0ca1b1f986b1ca
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-06-15 23:34:13 -0300

    Feat: Criação do `UserDeactivate`

---
+++ src/main/java/luis/goes/urlshortener/application/useCases/user/deactivate/UserDeactivate.java
@@ -0,0 +1,23 @@
+package luis.goes.urlshortener.application.useCases.user.deactivate;
+
+import jakarta.persistence.EntityNotFoundException;
+import luis.goes.urlshortener.infrastructure.repository.user.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UserDeactivate implements IUserDeactivateUseCase {
+    private final UserRepository repository;
+
+    public UserDeactivate(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void deactivate(UUID id) {
+        var user = repository.findById(id).orElseThrow(() -> new EntityNotFoundException("User not found"));
+        user.dateInfo.deactivate();
+        repository.save(user);
+    }
+}
