cba426c46dbcf2f14f2a0ec5d2f86e494cd08739
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-06-15 23:32:41 -0300

    Refactor: Renomeação de nome do use case que cria o usuário

--- src/main/java/luis/goes/urlshortener/application/useCases/user/IUserUseCases.java
+++
@@ -1,8 +0,0 @@
-package luis.goes.urlshortener.application.useCases.user;
-
-import luis.goes.urlshortener.domain.entity.user.UserEntity;
-
-public interface IUserUseCases {
-
-    UserEntity create(String name);
-}
---
+++ src/main/java/luis/goes/urlshortener/application/useCases/user/create/IUserCreateUseCase.java
@@ -0,0 +1,9 @@
+package luis.goes.urlshortener.application.useCases.user.create;
+
+import luis.goes.urlshortener.presentation.dtos.user.UserResponseDto;
+
+public interface IUserCreateUseCase {
+
+    UserResponseDto create(String name);
+
+}
