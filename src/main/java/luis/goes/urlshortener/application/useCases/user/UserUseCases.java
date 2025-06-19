57d0f77f98d5e725b18e82b8a7400ec02e77c0f5
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-06-19 14:40:54 -0300

    feat(UserUseCases): Adição do `@Getter`

--- src/main/java/luis/goes/urlshortener/application/useCases/user/UserUseCases.java
+++ src/main/java/luis/goes/urlshortener/application/useCases/user/UserUseCases.java
@@ -1,10 +1,12 @@
 package luis.goes.urlshortener.application.useCases.user;
 
+import lombok.Getter;
 import luis.goes.urlshortener.application.useCases.user.create.IUserCreateUseCase;
 import luis.goes.urlshortener.application.useCases.user.deactivate.IUserDeactivateUseCase;
 import org.springframework.stereotype.Component;
 
 @Component
+@Getter
 public class UserUseCases {
 
     private final IUserCreateUseCase userCreateUseCase;
@@ -13,14 +15,6 @@ public class UserUseCases {
     public UserUseCases(IUserCreateUseCase userCreateUseCase, IUserDeactivateUseCase userDeactivateUseCase) {
         this.userCreateUseCase = userCreateUseCase;
         this.userDeactivateUseCase = userDeactivateUseCase;
-    }
-
-    public IUserCreateUseCase getUserCreateUseCase() {
-        return userCreateUseCase;
-    }
-
-    public IUserDeactivateUseCase getUserDeactivateUseCase() {
-        return userDeactivateUseCase;
     }
 
 }
