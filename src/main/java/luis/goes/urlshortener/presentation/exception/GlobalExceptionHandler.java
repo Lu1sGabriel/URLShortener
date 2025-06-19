2d0080ad597bb7f36b29b0bf9a27b1723b9468fb
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-06-19 14:38:25 -0300

    feat(handler): adiciona tratamento para exceções de integridade, nulidade e JSON inválido

--- src/main/java/luis/goes/urlshortener/presentation/exception/GlobalExceptionHandler.java
+++ src/main/java/luis/goes/urlshortener/presentation/exception/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package luis.goes.urlshortener.presentation.exception;
 
+import luis.goes.urlshortener.shared.helpers.ConstraintNameMapper;
 import luis.goes.urlshortener.shared.helpers.StatusCode;
+import org.hibernate.PropertyValueException;
+import org.hibernate.exception.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,6 +24,50 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(ex.getStatusCode()).body(error);
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleUniqueConstrainViolationKeys(Exception ex) {
+        String constraintName = ((ConstraintViolationException) ex.getCause()).getConstraintName();
+
+        String fieldName = ConstraintNameMapper.getFieldName(constraintName);
+
+        LOGGER.warn("Handled JPA exception: {}", ex.getMessage());
+
+        ErrorResponse error = new ErrorResponse(
+                "Field '" + fieldName + "' already has a value",
+                StatusCode.CONFLICT
+        );
+
+        return ResponseEntity.status(error.statusCode()).body(error);
+    }
+
+
+    @ExceptionHandler(PropertyValueException.class)
+    public ResponseEntity<ErrorResponse> handleNullableException(PropertyValueException ex) {
+        LOGGER.error("Null value in not-null field", ex);
+
+        String property = ex.getPropertyName();
+        String entity = ex.getEntityName();
+
+        ErrorResponse error = new ErrorResponse(
+                String.format("The required field '%s' in entity '%s' was not provided.", property, entity),
+                StatusCode.BAD_REQUEST
+        );
+
+        return ResponseEntity.status(error.statusCode()).body(error);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleJsonInvalid(HttpMessageNotReadableException ex) {
+        LOGGER.error("Invalid JSON", ex);
+
+        ErrorResponse error = new ErrorResponse(
+                "The JSON sent in the request is invalid. Please check the syntax.",
+                StatusCode.BAD_REQUEST
+        );
+
+        return ResponseEntity.status(error.statusCode()).body(error);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
         LOGGER.error("Unhandled exception caught", ex);
@@ -26,9 +75,7 @@ public class GlobalExceptionHandler {
                 "Internal server error. That's not your fault!",
                 StatusCode.INTERNAL_SERVER_ERROR
         );
-        return ResponseEntity
-                .status(StatusCode.INTERNAL_SERVER_ERROR.getCode())
-                .body(error);
+        return ResponseEntity.status(error.statusCode()).body(error);
     }
 
 }
