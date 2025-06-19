a9776ff6b0d31fd47e07891dfa18653ab9f081b3
Luis Gabriel Goés <111465341+Lu1sGabriel@users.noreply.github.com>
2025-06-18 21:24:19 -0300

    Feat: Criação do `NameTest` para testar o validador

---
+++ src/test/java/luis/goes/urlshortener/name/NameTest.java
@@ -0,0 +1,30 @@
+package luis.goes.urlshortener.name;
+
+import luis.goes.urlshortener.shared.types.Name;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class NameTest {
+
+    @Test
+    void shouldCreateValidFullName() {
+        final String fullName = "Luis Gabriel Goés de Santana";
+        Name name = new Name(fullName);
+        assertEquals(fullName, name.name());
+    }
+
+    @Test
+    void shouldThrowExceptionForInvalidName() {
+        final String invalidName = "Luis123!";
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Name(invalidName);
+        });
+
+        assertThat(exception.getMessage(), CoreMatchers.containsString("Name must contain only letters and spaces"));
+    }
+}
